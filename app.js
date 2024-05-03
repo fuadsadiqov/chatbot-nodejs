95c5a8c15a4239251a697090f2d84c116f56588a
fuad.sadigov <fuad.sadigov@aih.az>
2024-05-03 14:25:12 +0400

    add some change

--- app.js
+++ app.js
@@ -1,61 +1,40 @@
-const express = require("express");
+const express = require('express');
+const CryptoJS = require('crypto-js');
+require('dotenv').config();
+const cors = require('cors');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
 const app = express();
-const port = process.env.PORT || 3001;
+app.use(cors());
+app.use(cors({
+  origin: process.env.URL
+}));
+app.use(express.json());
 
-app.get("/", (req, res) => res.type('html').send(html));
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.post('/message', async (req, res) => {
+  const { message } = req.body;
+  try {
+    const bytes = CryptoJS.AES.decrypt(message, process.env.SECRET_KEY);
+    if (bytes.sigBytes <= 0) {
+      throw new Error("Decryption failed. Possible encoding or key mismatch."); 
+    }
+    const decryptedRequest = bytes.toString(CryptoJS.enc.Utf8);
+    if (!decryptedRequest) {
+      throw new Error("Decryption resulted in empty string."); 
+    }
+    const model = genAI.getGenerativeModel({ model: "models/gemini-pro"});
+    const result = await model.generateContent(decryptedRequest);
+    const response = await result.response;
+    const text = await response.text();
+    const encryptedResponse = CryptoJS.AES.encrypt(text, process.env.SECRET_KEY).toString();
+    res.json({ message: encryptedResponse });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error processing your request');
+  }
+});
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
-
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
